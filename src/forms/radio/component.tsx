6c1067eead655c97577a70abf3afed33dec5f550
mwszekely <19625841+mwszekely@users.noreply.github.com>
2021-06-04 22:08:38 -0400

    Radio improvements

--- src/forms/radio/component.tsx
+++ src/forms/radio/component.tsx
@@ -1,5 +1,5 @@
 
-import { ComponentChildren, Fragment, h, Ref } from "preact";
+import { ComponentChildren, createContext, Fragment, h, Ref } from "preact";
 import { forwardElementRef, InputRadio, InputRadioProps, InputRadioGroup, InputRadioGroupProps } from "preact-async-input";
 import { useContext } from "preact/hooks";
 import { clsx } from "../../bootstrap-classes";
@@ -10,35 +10,42 @@ import { Label, ProvideLabel } from "../label";
 export interface RadioGroupPropsMin { }
 
 export interface RadioGroupProps extends RadioPropsMin, InputRadioGroupProps {
-
+    inline?: boolean;
 }
 
+const InlineContext = createContext(false);
+
 export function RadioGroup(props: RadioGroupProps) {
-    return (<InputRadioGroup {...props} />)
+    return (<InlineContext.Provider value={props.inline ?? false}><InputRadioGroup {...props} /></InlineContext.Provider>)
 }
 
 export interface RadioPropsMin { }
 
 export interface RadioProps extends RadioPropsMin, InputRadioProps {
     inline?: boolean;
-    label?: ComponentChildren;
+    children?: ComponentChildren;
 }
 
 export const Radio = forwardElementRef(function Radio(p: RadioProps, r: Ref<HTMLInputElement>) {
-    const { className, label, disabled, childrenPost, value, inline, ref, ...props } = { ...p, ref: r };
+    const { className, children, disabled, childrenPost, value, inline, ref, ...props } = { ...p, ref: r };
 
-    const isInInputGroup = useContext(IsInInputGroupContext);
+    const c = <InputRadio {...props} className={clsx("form-check-input")} value={value} ref={ref} />;
 
-    const c = <InputRadio {...props} className={clsx("form-check-input")} value={value} ref={ref} />
-    return (
-        <div className={clsx(isInInputGroup ? "input-group-text" : ["form-check", inline && "form-check-inline"], className)}>
-            {label == null ? c :
-                <ProvideLabel label={label} position="after">
-                    {c}
-                </ProvideLabel>
-            }
-        </div>
-    )
+    if (children == null) {
+        return <RadioOuter {...p}>{c}</RadioOuter>
+    }
+    else {
+        return <ProvideLabel label={children} position="after"><RadioOuter {...p}>{c}</RadioOuter></ProvideLabel>
+    }
 });
+
+const RadioOuter = ({ inline, className, children }: RadioProps) => {
+    const isInInputGroup = useContext(IsInInputGroupContext);
+    const defaultInline = useContext(InlineContext);
+    return (
+        <div className={clsx(isInInputGroup ? "input-group-text" : ["form-check", (inline ?? defaultInline) && "form-check-inline"], className)}>
+            {children}
+        </div>);
+}
 
 
